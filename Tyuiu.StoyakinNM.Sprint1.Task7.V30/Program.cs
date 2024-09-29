86081084a2b5e77aee5af53ebc83dd5d7ec56392
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-09-29 15:18:57 +0300

    Task 7

---
+++ Tyuiu.StoyakinNM.Sprint1.Task7.V30/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.StoyakinNM.Sprint1.Task7.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Стоякин Н.М. | ПКТб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнил: Стоякин Никита Михайлович | ПКТб 24-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу: пользователь вводит текст. Напечатать все слова,    *");
+        Console.WriteLine("* удалив из них последнюю букву.                                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y;
+
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ;                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x, y));
+        Console.ReadKey();
+
+    }
+}
